72fec9a31d45bb11f3953f28e5ad657b6c434de2
Kaushik Sarkar <akaushikarkar@gmail.com>
2025-03-19 20:27:06 +0530

    fix: update COOKIE_OPTIONS to set sameSite to false for improved compatibility

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -2,6 +2,6 @@ export const DB_NAME = "innovatix";
 export const PORT = 6969;
 export const COOKIE_OPTIONS = {
   httpOnly: true,
-  sameSite: true,
+  sameSite: false,
   secure: process.env.NODE_ENV === "production",
 };
